a09c6e6c7165387788047092c864cb80380e1b6b
Sergio Piana <spiana64@gmail.com>
2013-07-11 23:13:45 +0200

    migrate to module

--- serpics/warehouse/main/src/java/com/serpics/warehouse/persistence/dao/WarehouseFactory.java
+++
@@ -1,9 +0,0 @@
-package com.serpics.warehouse.persistence.dao;
-
-import com.serpics.core.persistence.dao.BaseFactory;
-
-public interface WarehouseFactory extends BaseFactory {
-	public double fetchAvailability( String sku);
-	
-
-}
--- serpics/warehouse/main/src/java/com/serpics/warehouse/service/WarehouseService.java
+++
@@ -1,9 +0,0 @@
-package com.serpics.warehouse.service;
-
-import com.serpics.warehouse.InventoryNotAvailableException;
-
-public interface WarehouseService {
-
-	public boolean checkInventory(String sku , double quantity);
-	public void updateInventory(String sku , double quantity) throws InventoryNotAvailableException;
-}
