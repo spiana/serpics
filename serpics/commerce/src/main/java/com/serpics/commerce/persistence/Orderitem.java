c1bcbca4db77c400b21540f8e18547fbf70e475e
Sergio Piana <spiana64@gmail.com>
2014-03-21 15:20:32 +0100

    Signed-off-by: Sergio Piana <spiana64@gmail.com>

--- serpics/commerce/src/main/java/com/serpics/commerce/persistence/Orderitem.java
+++ serpics/commerce/src/main/java/com/serpics/commerce/persistence/Orderitem.java
@@ -5,7 +5,6 @@ import java.util.Set;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -14,229 +13,229 @@ import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
-import com.serpics.base.persistence.Currency;
 import com.serpics.catalog.persistence.AbstractProduct;
-import com.serpics.membership.persistence.Store;
-import com.serpics.membership.persistence.User;
 
 @Entity
 @Table(name = "orderitems")
 public class Orderitem extends com.serpics.core.persistence.jpa.Entity implements Serializable {
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
-	@Column(name = "orderitems_id", unique = true, nullable = false)
-	private Long orderitemsId;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "orderitems_id", unique = true, nullable = false)
+    private Long orderitemsId;
 
-	@Column(name = "discount_amount", precision = 10, scale = 4)
-	private final Double discountAmount = new Double(0);
+    @Column(name = "discount_amount", precision = 10, scale = 4)
+    private Double discountAmount = new Double(0);
 
-	@Column(name = "discount_perc", precision = 5, scale = 2)
-	private double discountPerc;
+    @Column(name = "discount_perc", precision = 5, scale = 2)
+    private double discountPerc;
 
-	@Column(name = "discount_perc1", precision = 5, scale = 2)
-	private double discountPerc1;
-	
-	@Column(name = "discount_perc2", precision = 5, scale = 2)
-	private double discountPerc2;
-	
-	@Column(nullable = false, precision = 5, scale = 2)
-	private double quantity;
+    @Column(name = "discount_perc1", precision = 5, scale = 2)
+    private double discountPerc1;
 
-	@Column(name = "shipping_address_id")
-	private Long shippingAddressId;
+    @Column(name = "discount_perc2", precision = 5, scale = 2)
+    private double discountPerc2;
 
-	@Column(name = "product_id", nullable = true)
-	AbstractProduct product;
+    @Column(nullable = false, precision = 5, scale = 2)
+    private double quantity;
 
-	@Column(nullable = false, length = 250)
-	private String sku;
+    @Column(name = "shipping_address_id")
+    private Long shippingAddressId;
 
-	@Column(name = "sku_cost", precision = 10, scale = 4)
-	private Double skuCost = new Double(0);
+    @Column(name = "product_id", nullable = true)
+    AbstractProduct product;
 
-	@Column(name = "sku_description", length = 1000)
-	private String skuDescription;
+    @Column(nullable = false, length = 250)
+    private String sku;
 
-	@Column(name = "sku_net_price", nullable = false, precision = 10, scale = 4)
-	private Double skuNetPrice = new Double(0);
+    @Column(name = "sku_cost", precision = 10, scale = 4)
+    private Double skuCost = new Double(0);
 
-	@Column(name = "sku_price", nullable = false, precision = 10, scale = 4)
-	private Double skuPrice = new Double(0);
+    @Column(name = "sku_description", length = 1000)
+    private String skuDescription;
 
-	@Column(name = "shipping_cost", nullable = false, precision = 10, scale = 4)
-	private Double shippingCost = new Double(0);
+    @Column(name = "sku_net_price", nullable = false, precision = 10, scale = 4)
+    private Double skuNetPrice = new Double(0);
 
-	// bi-directional many-to-one association to Order
-	@ManyToOne
-	@JoinColumn(name = "orders_id", nullable = false)
-	private AbstractOrder order;
+    @Column(name = "sku_price", nullable = false, precision = 10, scale = 4)
+    private Double skuPrice = new Double(0);
 
-	// bi-directional many-to-one association to Shipmode
-	@ManyToOne
-	@JoinColumn(name = "shipmode_id")
-	private Shipmode shipmode;
+    @Column(name = "shipping_cost", nullable = false, precision = 10, scale = 4)
+    private Double shippingCost = new Double(0);
 
-	// bi-directional many-to-one association to Suborder
-	@ManyToOne
-	@JoinColumn(name = "suborders_id")
-	private Suborder suborder;
+    // bi-directional many-to-one association to Order
+    @ManyToOne
+    @JoinColumn(name = "orders_id", nullable = false)
+    private AbstractOrder order;
 
-	// bi-directional many-to-one association to OrderitemsAttribute
-	@OneToMany(mappedBy = "orderitem")
-	private Set<OrderitemsAttribute> orderitemsAttributes;
+    // bi-directional many-to-one association to Shipmode
+    @ManyToOne
+    @JoinColumn(name = "shipmode_id")
+    private Shipmode shipmode;
 
-	public Orderitem() {
-	}
+    // bi-directional many-to-one association to Suborder
+    @ManyToOne
+    @JoinColumn(name = "suborders_id")
+    private Suborder suborder;
 
-	public Orderitem(String sku, String sku_description, double quantity, Double price) {
-		this.sku = sku;
-		this.skuDescription = sku_description;
-		this.quantity = quantity;
-		this.skuCost = this.skuNetPrice = this.skuPrice = price;
+    // bi-directional many-to-one association to OrderitemsAttribute
+    @OneToMany(mappedBy = "orderitem")
+    private Set<OrderitemsAttribute> orderitemsAttributes;
 
-	}
+    public Orderitem() {
+    }
 
-	public Long getOrderitemsId() {
-		return this.orderitemsId;
-	}
+    public Orderitem(final String sku, final String sku_description, final double quantity, final Double price) {
+        this.sku = sku;
+        this.skuDescription = sku_description;
+        this.quantity = quantity;
+        this.skuCost = this.skuNetPrice = this.skuPrice = price;
 
-	public void setOrderitemsId(Long orderitemsId) {
-		this.orderitemsId = orderitemsId;
-	}
+    }
 
-	public double getDiscountPerc() {
-		return this.discountPerc;
-	}
+    public Long getOrderitemsId() {
+        return this.orderitemsId;
+    }
 
-	public void setDiscountPerc(double discountPerc) {
-		this.discountPerc = discountPerc;
-	}
+    public void setOrderitemsId(final Long orderitemsId) {
+        this.orderitemsId = orderitemsId;
+    }
 
-	public double getQuantity() {
-		return this.quantity;
-	}
+    public double getDiscountPerc() {
+        return this.discountPerc;
+    }
 
-	public void setQuantity(double quantity) {
-		this.quantity = quantity;
-	}
+    public void setDiscountPerc(final double discountPerc) {
+        this.discountPerc = discountPerc;
+    }
 
-	public Long getShippingAddressId() {
-		return this.shippingAddressId;
-	}
+    public double getQuantity() {
+        return this.quantity;
+    }
 
-	public void setShippingAddressId(Long shippingAddressId) {
-		this.shippingAddressId = shippingAddressId;
-	}
+    public void setQuantity(final double quantity) {
+        this.quantity = quantity;
+    }
 
-	public String getSku() {
-		return this.sku;
-	}
+    public Long getShippingAddressId() {
+        return this.shippingAddressId;
+    }
 
-	public void setSku(String sku) {
-		this.sku = sku;
-	}
+    public void setShippingAddressId(final Long shippingAddressId) {
+        this.shippingAddressId = shippingAddressId;
+    }
 
-	public AbstractOrder getOrder() {
-		return this.order;
-	}
+    public String getSku() {
+        return this.sku;
+    }
 
-	public void setOrder(AbstractOrder order) {
-		this.order = order;
-	}
+    public void setSku(final String sku) {
+        this.sku = sku;
+    }
 
-	public Shipmode getShipmode() {
-		return this.shipmode;
-	}
+    public AbstractOrder getOrder() {
+        return this.order;
+    }
 
-	public void setShipmode(Shipmode shipmode) {
-		this.shipmode = shipmode;
-	}
+    public void setOrder(final AbstractOrder order) {
+        this.order = order;
+    }
 
-	public Suborder getSuborder() {
-		return this.suborder;
-	}
+    public Shipmode getShipmode() {
+        return this.shipmode;
+    }
 
-	public void setSuborder(Suborder suborder) {
-		this.suborder = suborder;
-	}
+    public void setShipmode(final Shipmode shipmode) {
+        this.shipmode = shipmode;
+    }
 
-	public Set<OrderitemsAttribute> getOrderitemsAttributes() {
-		return this.orderitemsAttributes;
-	}
+    public Suborder getSuborder() {
+        return this.suborder;
+    }
 
-	public void setOrderitemsAttributes(Set<OrderitemsAttribute> orderitemsAttributes) {
-		this.orderitemsAttributes = orderitemsAttributes;
-	}
+    public void setSuborder(final Suborder suborder) {
+        this.suborder = suborder;
+    }
 
-	public Double getSkuCost() {
-		return skuCost;
-	}
+    public Set<OrderitemsAttribute> getOrderitemsAttributes() {
+        return this.orderitemsAttributes;
+    }
 
-	public void setSkuCost(Double skuCost) {
-		this.skuCost = skuCost;
-	}
+    public void setOrderitemsAttributes(final Set<OrderitemsAttribute> orderitemsAttributes) {
+        this.orderitemsAttributes = orderitemsAttributes;
+    }
 
-	public String getSkuDescription() {
-		return skuDescription;
-	}
+    public Double getSkuCost() {
+        return skuCost;
+    }
 
-	public void setSkuDescription(String skuDescription) {
-		this.skuDescription = skuDescription;
-	}
+    public void setSkuCost(final Double skuCost) {
+        this.skuCost = skuCost;
+    }
 
-	public Double getSkuNetPrice() {
-		return skuNetPrice;
-	}
+    public String getSkuDescription() {
+        return skuDescription;
+    }
 
-	public void setSkuNetPrice(Double skuNetPrice) {
-		this.skuNetPrice = skuNetPrice;
-	}
+    public void setSkuDescription(final String skuDescription) {
+        this.skuDescription = skuDescription;
+    }
 
-	public Double getSkuPrice() {
-		return skuPrice;
-	}
+    public Double getSkuNetPrice() {
+        return skuNetPrice;
+    }
 
-	public void setSkuPrice(Double skuPrice) {
-		this.skuPrice = skuPrice;
-	}
+    public void setSkuNetPrice(final Double skuNetPrice) {
+        this.skuNetPrice = skuNetPrice;
+    }
 
-	public Double getDiscountAmount() {
-		return discountAmount;
-	}
+    public Double getSkuPrice() {
+        return skuPrice;
+    }
 
-	public AbstractProduct getProduct() {
-		return product;
-	}
+    public void setSkuPrice(final Double skuPrice) {
+        this.skuPrice = skuPrice;
+    }
 
-	public void setProduct(AbstractProduct product) {
-		this.product = product;
-	}
+    public Double getDiscountAmount() {
+        return discountAmount;
+    }
 
-	public Double getShippingCost() {
-		return shippingCost;
-	}
+    public AbstractProduct getProduct() {
+        return product;
+    }
 
-	public void setShippingCost(Double shippingCost) {
-		this.shippingCost = shippingCost;
-	}
+    public void setProduct(final AbstractProduct product) {
+        this.product = product;
+    }
 
-	public double getDiscountPerc1() {
-		return discountPerc1;
-	}
+    public Double getShippingCost() {
+        return shippingCost;
+    }
 
-	public void setDiscountPerc1(double discountPerc1) {
-		this.discountPerc1 = discountPerc1;
-	}
+    public void setShippingCost(final Double shippingCost) {
+        this.shippingCost = shippingCost;
+    }
 
-	public double getDiscountPerc2() {
-		return discountPerc2;
-	}
+    public double getDiscountPerc1() {
+        return discountPerc1;
+    }
 
-	public void setDiscountPerc2(double discountPerc2) {
-		this.discountPerc2 = discountPerc2;
-	}
+    public void setDiscountPerc1(final double discountPerc1) {
+        this.discountPerc1 = discountPerc1;
+    }
 
+    public double getDiscountPerc2() {
+        return discountPerc2;
+    }
+
+    public void setDiscountPerc2(final double discountPerc2) {
+        this.discountPerc2 = discountPerc2;
+    }
+
+    public void setDiscountAmount(final Double discountAmount) {
+        this.discountAmount = discountAmount;
+    }
 
 }
