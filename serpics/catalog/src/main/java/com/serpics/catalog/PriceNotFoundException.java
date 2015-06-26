198698dfafb25b6ccf27e0aefd6e66e80ddbc4fb
Sergio Piana <spiana64@gmail.com>
2015-06-26 07:42:33 +0200

    adding PriceNotFoundException

---
+++ serpics/catalog/src/main/java/com/serpics/catalog/PriceNotFoundException.java
@@ -0,0 +1,14 @@
+package com.serpics.catalog;
+
+import com.serpics.catalog.data.model.AbstractProduct;
+
+public class PriceNotFoundException extends ProductNotFoundException {
+	private static final long serialVersionUID = -6351739784145669673L;
+
+	public PriceNotFoundException(AbstractProduct product) {
+		super(String.format("Price not forund for product %s", product.getCode()));
+	}
+
+	
+
+}
