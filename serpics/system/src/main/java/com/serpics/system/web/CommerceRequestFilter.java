994b21e3b731d9383d42310544dc92a91889aa6d
Sergio Piana <spiana@stepfour.it>
2015-08-23 17:03:08 +0200

    add servlet request filter which bind current serpics session on ssid addtribute

--- serpics/system/src/main/java/com/serpics/system/web/CommerceRequestFilter.java
+++ serpics/system/src/main/java/com/serpics/system/web/CommerceRequestFilter.java
@@ -2,6 +2,7 @@ package com.serpics.system.web;
 
 import java.io.IOException;
 
+import javax.annotation.Resource;
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
 import javax.servlet.FilterConfig;
@@ -14,17 +15,25 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import com.serpics.commerce.core.CommerceEngine;
+import com.serpics.membership.services.BaseService;
 
 public class CommerceRequestFilter implements Filter {
     private static final Logger logger = LoggerFactory.getLogger(CommerceRequestFilter.class);
 
+	@Resource
+	BaseService baseService;
+	
     @Autowired
     CommerceEngine ce;
 
     @Override
     public void doFilter(final ServletRequest req, final ServletResponse resp,
             final FilterChain chain) throws IOException, ServletException {
-        
+    	
+    	if (!baseService.isInitialized())
+	    	baseService.initIstance();
+    	
+    	
         chain.doFilter(req, resp);
         ce.unbind(); // unbind from commerce Session
     }
