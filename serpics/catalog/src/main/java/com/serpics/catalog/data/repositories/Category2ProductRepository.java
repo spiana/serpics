885d6f6c744909dff43bac3f396d4e7a93d91631
amarasco@stepfour.it <amarasco@stepfour.it>
2015-10-27 16:46:42 +0100

    modify method to retrieve list of products of one category

--- serpics/catalog/src/main/java/com/serpics/catalog/data/repositories/Category2ProductRepository.java
+++ serpics/catalog/src/main/java/com/serpics/catalog/data/repositories/Category2ProductRepository.java
@@ -2,11 +2,21 @@ package com.serpics.catalog.data.repositories;
 
 import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import com.serpics.catalog.data.model.Category;
 import com.serpics.catalog.data.model.CategoryProductRelation;
 import com.serpics.catalog.data.model.CtentryRelationPK;
 import com.serpics.catalog.data.model.Product;
 import com.serpics.core.data.Repository;
 
 public interface Category2ProductRepository extends Repository<CategoryProductRelation, CtentryRelationPK>  {
+	
+	@Query("select c.childProduct from CategoryProductRelation c where c.parentCategory = :category")
+	public Page<Product> findProductsByCategory(@Param("category") Category category,Pageable pageable);
+	
 	public List<CategoryProductRelation> findByChildProduct(Product product);
 }
