e89f306cd1bb6e5957bf29859c9d5b9d94ae0fb9
fpicinelli <fpicinelli@stepfour.it>
2015-11-13 15:24:17 +0100

    Aggiunta risposta Response al categoryRestService

--- serpics/rest-api/src/main/java/com/serpics/jaxrs/CategoryRestService.java
+++ serpics/rest-api/src/main/java/com/serpics/jaxrs/CategoryRestService.java
@@ -1,25 +1,20 @@
 package com.serpics.jaxrs;
 
-import java.util.List;
-
 import javax.ws.rs.core.Response;
 
-import org.springframework.data.domain.Page;
-
 import com.serpics.catalog.facade.data.CategoryData;
-import com.serpics.catalog.facade.data.ProductData;
 
 public interface CategoryRestService {
 	
 	public Response createParent(CategoryData category, Long parentId);
 	public Response create(CategoryData category);
 	public Response addParent(Long childId, Long parentId);
-	public List<CategoryData> getChild(Long parentId);
-	public CategoryData getCategoryById(Long categoryId);
-	public CategoryData getCategoryByCode(String categoryId);
+	public Response getChild(Long parentId);
+	public Response getCategoryById(Long categoryId);
+	public Response getCategoryByCode(String categoryId);
 	public Response update(CategoryData category);
 	public Response delete(Long categoryId);
-	public Page<CategoryData> findAll(int page, int size);
-	public List<CategoryData> getTop();
+	public Response findAll(int page, int size);
+	public Response getTop();
 	
 }
