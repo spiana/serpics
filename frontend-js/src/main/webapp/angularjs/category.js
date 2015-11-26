d61655f4bd5ab4e11eb450deb2774e1377668aca
christian <chiama@stepfour.it>
2015-11-26 17:51:28 +0100

    complete category service/controller angular with initialm documentation (the code)

--- frontend-js/src/main/webapp/angularjs/category.js
+++ frontend-js/src/main/webapp/angularjs/category.js
@@ -1,9 +1,9 @@
-var app = angular.module("category", ['AuthManager'])
+var app = angular.module("category", ['AuthManager','ngCookies'])
 
 .constant('api_endpoint', "http://localhost:8080/jax-rs/categoryService/")
 
- app.service("categoryService",['authManager',
-            function( $http, $q ,authManager,api_endpoint) {
+ app.service("categoryService",
+            function( $http, $q, api_endpoint) {
 	 
                 /** Return public API. (like java interface)**/
                 var service = ({
@@ -22,7 +22,11 @@ var app = angular.module("category", ['AuthManager'])
                 
                 /** public methods**/
                 /** create **/
-                function createCategory( data ) {
+                /**
+                 * @param sessionId
+                 * @param data
+                 */
+                function createCategory(sessionId, data ) {
                     var request = $http({
                         method: 'POST',
                         url: api_endpoint + 'create',
@@ -34,8 +38,12 @@ var app = angular.module("category", ['AuthManager'])
                     return( request.then( handleSuccess, handleError ) );
                 }
                 
-                /** create **/
-                function createParent( parent , data ) {
+                /**
+                 * @param sessionId
+                 * @param parent
+                 * @param data
+                 */
+                function createParent(sessionId, parent , data ) {
                     var request = $http({
                         method: 'POST',
                         url: api_endpoint + parent,
@@ -47,8 +55,14 @@ var app = angular.module("category", ['AuthManager'])
                     return( request.then( handleSuccess, handleError ) );
                 }
                 
-                /** create **/
-                function addParent( child,parent,data ) {
+                /**
+                 * @param sessionId
+                 * @param child
+                 * @param parent
+                 * @param data
+                 * @return 
+                 */
+                function addParent(sessionId, child,parent,data ) {
                     var request = $http({
                         method: 'POST',
                         url: api_endpoint + 'addParent/' + child +'/' + parent,
@@ -60,8 +74,12 @@ var app = angular.module("category", ['AuthManager'])
                     return( request.then( handleSuccess, handleError ) );
                 }
                 
-                /** update **/
-                function updateCategory( data ) {
+                /**
+                 * @param sessionId                
+                 * @param data
+                 * @return 
+                 */
+                function updateCategory(sessionId, data ) {
                     var request = $http({
                         method: 'PUT',
                         url: api_endpoint + 'update',
@@ -73,8 +91,12 @@ var app = angular.module("category", ['AuthManager'])
                     return( request.then( handleSuccess, handleError ) );
                 }
                 
-                /** delete **/
-                function deleteCategory(categoryId) {
+                /**
+                 * @param sessionId
+                 * @param categoryId            
+                 * @return 
+                 */
+                function deleteCategory(sessionId,categoryId) {
                     var request = $http({
                         method: 'DELETE',
                         url: api_endpoint + 'delete/' + categoryId,
@@ -86,20 +108,27 @@ var app = angular.module("category", ['AuthManager'])
                 }
                 
                
-                /** read **/      
-                function getTop() {
+                /**
+                 * @param sessionId                
+                 * @return 
+                 */     
+                function getTop(sessionId) {
                 	 var request = $http({
                          method: 'GET',
                          url	: 	api_endpoint + 'top',
                          headers: {
-                         	'ssid': authManager.getSessionId
+                         	'ssid': sessionId
                          }                            
                        });
                     return( request.then( handleSuccess, handleError ) );
                 }
                 
-                /** read **/      
-                function getCategoryById(categoryId) {
+                /**
+                 * @param sessionId
+                 * @param categoryId                
+                 * @return 
+                 */      
+                function getCategoryById(sessionId,categoryId) {
                 	 var request = $http({
                          method: 	'GET',
                          url: 		api_endpoint  + categoryId,
@@ -110,8 +139,13 @@ var app = angular.module("category", ['AuthManager'])
                     return( request.then( handleSuccess, handleError ) );
                 }
                 
-                /** read **/      
-                function getCategoryByCode(code,category) {
+                /**
+                 * @param sessionId
+                 * @param code
+                 * @param category                
+                 * @return 
+                 */      
+                function getCategoryByCode(sessionId,code,category) {
                 	 var request = $http({
                          method: 	'GET',
                          url: 		api_endpoint +  code + '/' + category,
@@ -122,8 +156,12 @@ var app = angular.module("category", ['AuthManager'])
                     return( request.then( handleSuccess, handleError ) );
                 }
                 
-                /** read **/      
-                function getChild(parent) {
+                /**
+                 * @param sessionId               
+                 * @param parent                 
+                 * @return 
+                 */      
+                function getChild(sessionId,parent) {
                 	 var request = $http({
                          method: 	'GET',
                          url: 		api_endpoint + 'getChild/' + parent,
@@ -134,11 +172,14 @@ var app = angular.module("category", ['AuthManager'])
                     return( request.then( handleSuccess, handleError ) );
                 }
                 
-                /** read **/      
-                function findAll() {
+                /**
+                 * @param sessionId               
+                 * @return 
+                 */     
+                function findAll(sessionId) {
                 	 var request = $http({
                          method: 	'GET',
-                         url: 		api_endpoint + 'findAll',
+                         url: 	/** read **/ 	api_endpoint + 'findAll',
                          headers: {
                          	'ssid': authManager.getsessionId
                          }                         
@@ -174,4 +215,139 @@ var app = angular.module("category", ['AuthManager'])
                     return( response.data );
                 }
             }
-        ]);
+        );
+
+
+app.controller("categoryController",['$scope','$cookies','authManagerService','categoryService', 
+                                     
+    function($scope,$cookies,authManagerService,categoryService) {	
+ 	
+	    var endpoint    = 'http://localhost:8080/jax-rs/auth/connect/default-store'
+		var category 	= []
+	  
+	    
+	    /** implemented service category **/ 
+	    
+	    /**
+	     * @param ssid 		a sessionId
+	     * @return 			all category pather
+	     * @use 			categoryService,authManagerService
+	     */
+		$scope.getTop = function(ssid) {		
+	       	categoryService.getTop(sessionId).then( function( response ) {
+            	/** do stuff with response **/
+            })
+	    };
+	    
+	    /**
+	     * @param ssid 			a sessionId
+	     * @param categoryId 	category id to be retrive
+	     * @return 				a category by id
+	     * @use 				categoryService,authManagerService
+	     */
+	    $scope.getCategoryById = function(sessionId,categoryId) {		
+	       	categoryService.getCategoryById(sessionId,categoryId).then( function( response ) {
+	       		/** do stuff with response **/
+            })
+	    };
+	    
+	    /**
+	     * @param ssid 			a sessionId
+	     * @param 				code 
+	     * @param category 		category to retrieve
+	     * @return 				a category by code
+	     * @use 				categoryService,authManagerService
+	     */
+	    $scope.getCategoryByCode = function(sessionId,code,category) {		
+	       	categoryService.getCategoryByCode(sessionId,code,category).then( function( response ) {
+	       		/** do stuff with response **/
+            })
+	    };
+	    
+	    /**
+	     * @param ssid 		a sessionId
+	     * @param parent 	a parent category
+	     * @return 			all category child
+	     * @use 			categoryService,authManagerService
+	     */
+	    $scope.getChild = function(sessionId,parent) {		
+	       	categoryService.getChild(sessionId,parent).then( function( response ) {
+	       		/** do stuff with response **/
+            })
+	    };
+	    
+	    /**
+	     * @param ssid 		a sessionId
+	     * @return 			all category 
+	     * @use 			categoryService,authManagerService
+	     */
+	    $scope.findAll = function(sessionId) {		
+	       	categoryService.findAll(sessionId).then( function( response ) {
+	       		/** do stuff with response **/
+            })
+	    };
+	    
+	    /**
+	     * @param ssid 		a sessionId
+	     * @param data 		data to send
+	     * @return 			new category
+	     * @use 			categoryService,authManagerService
+	     */
+	    $scope.createCategory = function(sessionId,data) {		
+	       	categoryService.createCategory(sessionId, data ).then( function( response ) {
+	       		/** do stuff with response **/
+            })
+	    };
+	    
+	    /**
+	     * @param ssid 		a sessionId
+	     * @param parent 	a parent category
+	     * @param data 		data to send
+	     * @return 			add parent to category
+	     * @use 			categoryService,authManagerService
+	     */
+	    $scope.createParent = function(sessionId,parent,data) {		
+	       	categoryService.createParent(sessionId, parent , data ).then( function( response ) {
+	       		/** do stuff with response **/
+            })
+	    };
+	    
+	    /**
+	     * @param ssid 		a sessionId
+	     * @param child 	a sessionId
+	     * @param parent 	a parent category id
+	     * @param data 		data to send
+	     * @return 			add parent to category of id @child
+	     * @use 			categoryService,authManagerService
+	     */
+	    $scope.addParent = function(sessionId, child,parent,data) {		
+	       	categoryService.addParent(sessionId, child,parent,data ).then( function( response ) {
+	       		/** do stuff with response **/
+            })
+	    };
+	    
+	    /**
+	     * @param ssid 		a sessionId
+	     * @param data 		data to send
+	     * @return 			a category updated with @data params
+	     * @use 			categoryService,authManagerService
+	     */
+	    $scope.updateCategory = function(sessionId,data) {		
+	       	categoryService.updateCategory(sessionId, data ).then( function( response ) {
+	       		/** do stuff with response **/
+            })
+	    };
+	    
+	    /**
+	     * @param ssid 			a sessionId
+	     * @param categoryId 	a category id
+	     * @return delete 		a category with id @categoryId
+	     * @use 				categoryService,authManagerService
+	     */
+	    $scope.deleteCategory = function(sessionId,categoryId) {		
+	       	categoryService.deleteCategory(sessionId,categoryId).then( function( response ) {
+	       		/** do stuff with response **/
+            })
+	    };
+}])
+ 
