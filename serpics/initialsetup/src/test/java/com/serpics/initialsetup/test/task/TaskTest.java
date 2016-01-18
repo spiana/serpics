9862e8ae12ff58261fe46b23610911784622fab7
amarasco@stepfour.it <amarasco@stepfour.it>
2016-01-18 16:40:49 +0100

    add test for initialSetup

--- serpics/initialsetup/src/test/java/com/serpics/initialsetup/test/task/TaskTest.java
+++ serpics/initialsetup/src/test/java/com/serpics/initialsetup/test/task/TaskTest.java
@@ -1,21 +1,26 @@
-package com.serpics.initialsetup.service.test.task;
+package com.serpics.initialsetup.test.task;
 
+import java.io.InputStreamReader;
+
+import com.serpics.catalog.data.model.Product;
 import com.serpics.initialsetup.annotation.SystemSetupTaskConfig;
 import com.serpics.initialsetup.task.AbstractSystemTask;
 
-@SystemSetupTaskConfig(order=1)
+@SystemSetupTaskConfig(order=1,description="Test Task One")
 public class TaskTest extends AbstractSystemTask {
 
 	@Override
 	public void doExecuteProjectData() {
-		System.out.println("doExecuteProjectData");
+		getImportCsvService().importCsv(new InputStreamReader(this.getClass().getClassLoader()
+                .getResourceAsStream("productProjectTest1.csv")), Product.class);
 
 	}
 
 	@Override
 	public void doExecuteSampleData() {
-		System.out.println("doExecuteSampleData");
+		getImportCsvService().importCsv(new InputStreamReader(this.getClass().getClassLoader()
+                .getResourceAsStream("productSampleTest1.csv")), Product.class);
 
 	}
-
+	
 }
--- serpics/initialsetup/src/test/java/com/serpics/initialsetup/test/task/TaskTest2.java
+++ serpics/initialsetup/src/test/java/com/serpics/initialsetup/test/task/TaskTest2.java
@@ -1,20 +1,25 @@
-package com.serpics.initialsetup.service.test.task;
+package com.serpics.initialsetup.test.task;
 
+import java.io.InputStreamReader;
+
+import com.serpics.catalog.data.model.Product;
 import com.serpics.initialsetup.annotation.SystemSetupTaskConfig;
 import com.serpics.initialsetup.task.AbstractSystemTask;
 
-@SystemSetupTaskConfig(order=2)
+@SystemSetupTaskConfig(order=2,description="Test Task Two")
 public class TaskTest2 extends AbstractSystemTask {
 
 	@Override
 	public void doExecuteProjectData() {
-		System.out.println("doExecuteProjectData");
+		getImportCsvService().importCsv(new InputStreamReader(this.getClass().getClassLoader()
+                .getResourceAsStream("productProjectTest2.csv")), Product.class);
 
 	}
 
 	@Override
 	public void doExecuteSampleData() {
-		System.out.println("doExecuteSampleData");
+		getImportCsvService().importCsv(new InputStreamReader(this.getClass().getClassLoader()
+                .getResourceAsStream("productSampleTest2.csv")), Product.class);
 
 	}
 
