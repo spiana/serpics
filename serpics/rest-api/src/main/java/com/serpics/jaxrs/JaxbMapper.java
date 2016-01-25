9323286cf7f5fc34457d3a603ec53065ad5885d2
Gabriele <ggiubelli@stepfour.it>
2016-01-25 12:20:20 +0100

    fix beans jax-rs and rest-api with fasterxml provider

--- serpics/rest-api/src/main/java/com/serpics/jaxrs/JaxbMapper.java
+++ serpics/rest-api/src/main/java/com/serpics/jaxrs/JaxbMapper.java
@@ -3,26 +3,33 @@ package com.serpics.jaxrs;
 import java.text.SimpleDateFormat;
 import java.util.TimeZone;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.AnnotationIntrospector;
+import com.fasterxml.jackson.databind.MapperFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.annotation.JsonSerialize.Inclusion;
-import com.fasterxml.jackson.module.jaxb.JaxbAnnotationIntrospector;
+import com.fasterxml.jackson.databind.introspect.JacksonAnnotationIntrospector;
 
 public class JaxbMapper  extends ObjectMapper{
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 4414515344211684147L;
+
 	public JaxbMapper() {
 		super();
-//		AnnotationIntrospector introspector2 = new JaxbAnnotationIntrospector();
-//		//AnnotationIntrospector introspector1 = new JacksonAnnotationIntrospector();
-//		//AnnotationIntrospector pair = new AnnotationIntrospector.Pair(introspector1, introspector2);				
-//		super.setAnnotationIntrospector(introspector2);
-////		configure(SerializationConfig.Feature.USE_ANNOTATIONS, true);
-////		configure(SerializationConfig.Feature.AUTO_DETECT_FIELDS , true);
-////		configure(SerializationConfig.Feature.AUTO_DETECT_GETTERS, true);
-////		setSerializationInclusion(Inclusion.NON_NULL);
-//		SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss z");
-//		dateFormat.setTimeZone(TimeZone.getTimeZone("GMT"));
-//		setDateFormat(dateFormat);
+		AnnotationIntrospector introspector2 = new JacksonAnnotationIntrospector();
+//		AnnotationIntrospector introspector1 = new JacksonAnnotationIntrospector();
+//		AnnotationIntrospector pair = new AnnotationIntrospector.Pair(introspector2, introspector1);				
+		super.setAnnotationIntrospector(introspector2);
+		configure(MapperFeature.USE_ANNOTATIONS, true);
+		configure(MapperFeature.AUTO_DETECT_FIELDS , true);
+		configure(MapperFeature.AUTO_DETECT_GETTERS, true);
+//		setSerializationInclusion(Inclusion.NON_NULL);
+		setSerializationInclusion(JsonInclude.Include.NON_NULL);
+		SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss z");
+		dateFormat.setTimeZone(TimeZone.getTimeZone("GMT"));
+		setDateFormat(dateFormat);
 		
 	}
 }
